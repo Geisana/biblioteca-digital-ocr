ff6ca5b58431ea10d4b53287e7f9be0711cdc60c
Geisana <frgeisana@gmail.com>
2026-05-22 01:49:12 -0300

    Criando tabelas automaticamente

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
+
+
 # ============================================
 # PASTAS
 # ============================================
@@ -490,9 +490,8 @@ def logout():
 # START
 # ============================================
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-
-    with app.app_context():
-        db.create_all()
-
     app.run(debug=True)
