3e8207a8dfb4ac0e6bd2ca0e474e1dfddc8b4ba9
Geisana <frgeisana@gmail.com>
2026-05-27 21:40:42 -0300

    Build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
+
 apt-get update
-apt-get install -y poppler-utils tesseract-ocr
+
+apt-get install -y \
+    tesseract-ocr \
+    tesseract-ocr-por \
+    poppler-utils
 
 pip install -r requirements.txt
